1817248f37c8a8a3d420635b30bffd00149ea072
Israel Cohen <josh50170@gmail.com>
2024-12-24 17:20:39 +0200

    fix: Delete comments from main function
    Signed-off-by: Israel Cohen <josh50170@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,15 +9,5 @@ int main()
 	GameTest::enterToFullCellTest();
 	GameTest::outOfRnageTest();
 
-	// bool keepPlay = true;
-
-	//while (keepPlay)
-	{
-		
-		// Game game(10, 8);
-		// game.play();
-		// std::cout << "Enter 1 to keep play and 0 to stop ";
-		// std::cin >> keepPlay;
-	}
-	
+	return 0;
 }
